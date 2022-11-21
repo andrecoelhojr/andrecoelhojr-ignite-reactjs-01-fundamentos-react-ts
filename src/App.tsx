b48840a2003c7ed49e7b8390aed4258e8f2023f1
Andre Coelho <andre.junior@adp.com>
2022-11-21 16:08:44 -0300

    Aprimorando a aplicação - TypeScript no React

---
+++ src/App.tsx
@@ -0,0 +1,70 @@
+import { Header } from "./components/Header";
+import { Post } from "./components/Post";
+import { Sidebar } from "./components/Sidebar";
+
+import styles from "./App.module.css";
+
+import "./global.css";
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/andrecoelhojr.png",
+      name: "André Coelho",
+      role: "Tech Lead @ Zallpy Digital",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galera 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2022-08-12 20:20:20"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/parkeranac.png",
+      name: "Ana Carolina",
+      role: "Empresária @ LuaraStoreCb",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galera 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de adicionar novidades no site da LuaraStoreCB, corram pois são peças exclusivas da nossa nova coleção de inverno 2022!! <3",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2022-08-18 15:15:15"),
+  },
+];
+
+export function App() {
+  return (
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          {posts.map((post) => {
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            );
+          })}
+        </main>
+      </div>
+    </div>
+  );
+}
